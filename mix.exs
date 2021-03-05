a323ac15025d299738e164331493a8c4c63fe120
Anastasiya Dyachenko <apelsinka223@gmail.com>
2021-03-06 01:26:38 +0300

    version 3.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule TestMatch.MixProject do
   def project do
     [
       app: :test_match,
-      version: "2.0.3",
+      version: "3.0.0",
       elixir: "~> 1.5",
       build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
