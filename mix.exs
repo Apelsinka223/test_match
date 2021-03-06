180c7df16a131576d51106ecf7c212674d3003fe
Anastasiya Dyachenko <apelsinka223@gmail.com>
2021-03-06 15:19:34 +0300

    update mix elixir version

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule TestMatch.MixProject do
     [
       app: :test_match,
       version: "3.0.0",
-      elixir: "~> 1.5",
+      elixir: "~> 1.10",
       build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
       description: description(),
